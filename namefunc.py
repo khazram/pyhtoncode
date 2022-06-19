a8c5f44b9d4d4050e6147b1a1803dd7757fa89aa
khazram <khazrariasat@gmail.com>
2022-06-18 21:24:32 -0400

    name,age.dob

---
+++ namefunc.py
@@ -0,0 +1,6 @@
+Name1,Age1,DOB= "ali","23","Feb2"
+Name2,Age2,DOB= "sara", "19","jan2"
+def printinfo(N,A,DOB):
+    print(N+" "+ A + " "+DOB)
+printinfo (Name1,Age1,DOB)    
+printinfo (Name2,Age2,DOB)
